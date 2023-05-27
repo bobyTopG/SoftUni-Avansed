fca4f6f9e5a73b39d099d6ae19e3cc3292e24ea6
Boyan Georgiev <106580794+bobyTopG@users.noreply.github.com>
2023-05-27 15:57:22 +0300

    Name Age

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+Stack<int> index = new Stack<int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '(')
+    {
+        index.Push(i);
+    }
+    else if(input[i] == ')')
+    {
+        int indexOfOpenBracket = index.Pop();
+
+        for (int j = indexOfOpenBracket; i >= j; j++)
+        {
+            Console.Write(input[j]);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ ex Person/ex Person/Program.cs
@@ -0,0 +1,64 @@
+﻿List<Person> people = new();
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string[] tocken = Console.ReadLine().Split(", ").ToArray();
+    string name = tocken[0];
+    int age = int.Parse(tocken[1]);
+
+    people.Add(new Person() {Age = age, Name = name });
+}
+
+string youngOrOld = Console.ReadLine();
+int ageFilter = int.Parse(Console.ReadLine());
+string condition = Console.ReadLine();
+
+Func<Person, bool> filter = GetFilter(youngOrOld, ageFilter);
+
+people = people.Where(filter).ToList();
+Action<Person> printer = Printer(condition);
+
+
+foreach (var person in people)
+{
+    printer(person);
+}
+
+Action<Person> Printer(string condition)
+{
+    switch (condition)
+    {
+        case "name age":return p =>
+        Console.WriteLine($"{p.Name} - {p.Age}");
+
+        case "name": return p =>
+        Console.WriteLine($"{p.Name}");
+
+        case "age": return p =>
+        Console.WriteLine($"{p.Age}");
+
+        default:return null;
+        
+    }
+}
+
+Func<Person, bool> GetFilter(string youngOrOld, int ageFilter)
+{
+    switch (youngOrOld)
+    {
+        case "younger": return person => person.Age < ageFilter;
+        case "older": return person => person.Age >= ageFilter;
+
+        default:
+            return null;
+    }
+       
+}
+
+class Person
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+   
+}
