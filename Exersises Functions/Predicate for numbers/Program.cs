2d6d0af5e744425d901b4d4de341fbc324432506
Boyan Georgiev <106580794+bobyTopG@users.noreply.github.com>
2023-05-27 23:41:55 +0300

    n
    n

---
+++ Exersises Functions/Predicate for numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int lenght = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+Action<string[], Predicate<string>> print = (names, mach) =>
+{
+    foreach (var name in names)
+    {
+        if (mach(name))
+        {
+            Console.WriteLine(name);
+        }
+    }
+};
+
+print(names, n => n.Length <= lenght);
