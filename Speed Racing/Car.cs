6baab3170e98ad866c8601965bdc2338f6a277fe
Boyan Georgiev <106580794+bobyTopG@users.noreply.github.com>
2023-06-01 09:40:17 +0300

    Speed Racing

---
+++ Speed Racing/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmont;
+        private double fuelConsumptionPerKilometer;
+        private double travelleddistance;
+
+
+
+        public Car()
+        {
+
+        }
+
+        public Car(string model, double fuelAmont, double fuelConsuption) : this()
+        {
+            Model = model;
+            FuelAmont = fuelAmont;
+            FuelConsumptionPerKilometer = fuelConsuption;
+        }
+
+
+        public string Model { get { return model; } set { model = value; } }
+        public double FuelAmont { get { return fuelAmont; } set { fuelAmont = value; } }
+        public double FuelConsumptionPerKilometer { get { return fuelConsumptionPerKilometer; } set { fuelConsumptionPerKilometer = value; } }
+        public double Travelleddistance { get { return travelleddistance; } set { travelleddistance = value; } }
+
+
+        public void Drive(double distanse)
+        {
+
+            double fuelToReduse = distanse * FuelConsumptionPerKilometer;
+            if (FuelAmont > fuelToReduse)
+            {
+                FuelAmont -= fuelToReduse;
+                Travelleddistance += distanse;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+
+
+    
+
+}
---
+++ Speed Racing/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SpeedRacing
+{
+    public class StartUp
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Dictionary<string, Car> dict = new();
+            string comand = string.Empty;
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] tockens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string model = tockens[0];
+                double fuelAmont = double.Parse(tockens[1]);
+                double fuelConsumption = double.Parse(tockens[2]);
+
+                Car car = new Car(model, fuelAmont, fuelConsumption);
+
+                dict.Add(car.Model, car);
+               
+            }
+
+            while ((comand = Console.ReadLine()) != "End")
+            {
+                string[] comandTockens = comand.Split();
+                string model = comandTockens[1];
+                double distanse = double.Parse(comandTockens[2]);
+
+                Car car1 = dict[model];
+                car1.Drive(distanse);
+            }
+
+            foreach (var (name , car) in dict)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmont} {car.Travelleddistance}");
+            }
+            
+
+        }
+    }
+}
+
+
