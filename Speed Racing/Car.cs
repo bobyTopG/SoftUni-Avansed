621184cb9ab6a56f2a9e66a4efe10202e9e57187
Boyan Georgiev <106580794+bobyTopG@users.noreply.github.com>
2023-06-01 09:42:50 +0300

    Update Car.cs

--- Speed Racing/Car.cs
+++ Speed Racing/Car.cs
@@ -9,20 +9,6 @@ namespace SpeedRacing
         private double travelleddistance;
 
 
-
-        public Car()
-        {
-
-        }
-
-        public Car(string model, double fuelAmont, double fuelConsuption) : this()
-        {
-            Model = model;
-            FuelAmont = fuelAmont;
-            FuelConsumptionPerKilometer = fuelConsuption;
-        }
-
-
         public string Model { get { return model; } set { model = value; } }
         public double FuelAmont { get { return fuelAmont; } set { fuelAmont = value; } }
         public double FuelConsumptionPerKilometer { get { return fuelConsumptionPerKilometer; } set { fuelConsumptionPerKilometer = value; } }
