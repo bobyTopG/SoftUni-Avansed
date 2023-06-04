e6ab7fbafa9cee774ed5b47455f392572418e977
Boyan Georgiev <106580794+bobyTopG@users.noreply.github.com>
2023-06-04 21:44:12 +0300

    Test

---
+++ RubberDuck1/RubberDuck1/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] timeArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] tasksArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> time = new(timeArray);
+Stack<int> tasks = new(tasksArray);
+
+int DarthVaderDucky = 0;
+int ThorDucky = 0;
+int BigBlueRubberDucky = 0;
+int SmallYellowRubberDucky = 0;
+
+for (int i = 0; i < timeArray.Length; i++)
+{
+
+    int total = time.Peek() * tasks.Peek();
+    int currentTask = tasks.Peek();
+    while (total > 240)
+    {
+        time.Enqueue(time.Dequeue());
+        currentTask -= 2;
+        total = time.Peek() * (currentTask);
+
+    }
+    if(total <= 60)
+    {
+        DarthVaderDucky++;
+    }
+    else if(total <= 120)
+    {
+        ThorDucky++;
+    }
+    else if(total <= 180)
+    {
+        BigBlueRubberDucky++;
+    }
+    else if (total <= 240)
+    {
+        SmallYellowRubberDucky++;
+    }
+    time.Dequeue();
+    tasks.Pop();
+}
+
+Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+Console.WriteLine($"Darth Vader Ducky: {DarthVaderDucky}\nThor Ducky: { ThorDucky}\nBig Blue Rubber Ducky: {BigBlueRubberDucky}\nSmall Yellow Rubber Ducky: {SmallYellowRubberDucky}");
---
+++ TheSquirrel2/TheSquirrel2/Program.cs
@@ -0,0 +1,100 @@
+﻿
+int row = 0;
+int col = 0;
+int hazelNut = 0;
+
+bool endOfGame = false;
+
+int n = int.Parse(Console.ReadLine());
+string[] tockens = Console.ReadLine().Split(", ").ToArray();
+
+char[,] field = new char[n, n];
+
+
+
+for (int i = 0; i < field.GetLength(0); i++)
+{
+    string sentence = Console.ReadLine();
+    char[] comand = sentence.ToCharArray();
+
+    for (int j = 0; j < field.GetLength(1); j++)
+    {
+        if (comand[j] == 's')
+        {
+            row = i;
+            col = j;
+        }
+        field[i, j] = comand[j];
+    }
+}
+
+   
+    for (int k = 0; k < tockens.Length; k++)
+    {
+        string comand = tockens[k];
+        field[row, col] = '*';
+
+        if (comand == "left")
+        {
+            col--;
+        }
+        else if (comand == "right")
+        {
+            col++;
+        }
+
+        else if (comand == "up")
+        {
+            row--;
+        }
+        else if (comand == "down")
+        {
+            row++;
+        }
+
+
+        if (row >= n || row < 0 || col >= n || col < 0)
+        {
+            Console.WriteLine("The squirrel is out of the field.");
+            endOfGame = true;
+            break;
+        }
+
+        else if (field[row, col] == '*')
+        {
+            continue;
+        }
+
+        else if (field[row, col] == 'h')
+        {
+            hazelNut++;
+            field[row, col] = '*';
+
+            if (hazelNut == 3)
+            {
+                Console.WriteLine("Good job! You have collected all hazelnuts!");
+                field[row, col] = 's';
+
+                endOfGame = true;
+                break;
+
+            }
+        }
+
+        else if (field[row, col] == 't')
+        {
+            Console.WriteLine($"Unfortunately, the squirrel stepped on a trap...");
+        field[row, col] = '*';
+        endOfGame = true;
+            break;
+        }
+
+
+    }
+if (!endOfGame)
+{
+    Console.WriteLine("There are more hazelnuts to collect.");
+}
+Console.WriteLine($"Hazelnuts collected: {hazelNut}");
+
+
