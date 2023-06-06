5d13448a1342f438d5a9fd97d82c132f195458bb
boyan <bg89262704@edu.mon.bg>
2023-06-06 22:15:31 +0300

    Box Generics
    Box Generic

---
+++ Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Box
+{
+	public class Box<T>
+	{
+		private List<T> items;
+		
+
+		public Box()
+		{
+			items = new List<T>();
+		}
+	
+		public void Add(T item)
+		{
+			items.Add(item);
+		}
+
+		public T Remove()
+		{
+			T firstItem = items[Count -1];
+			items.RemoveAt(items.Count-1);
+			return firstItem;
+		}
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+    }
+}
+
---
+++ Box/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Box;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Box<int> box = new Box<int>();
+        box.Add(1);
+        
+        Console.WriteLine(box.Remove());
+        box.Add(2);
+        box.Add(3);
+        box.Add(4);
+        Console.WriteLine(box.Remove());
+        Console.WriteLine(box.Count);
+    }
+}
+
